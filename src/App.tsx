13dd88588de57396d6fad9a4c8170f704ac1c8c0
Lucas Serral <lucasser21@gmail.com>
2022-12-20 23:29:28 -0300

    [ADD] SideBar in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <div className="App">
+      <SideBar />
       <p>OPEN CV</p>
     </div>
   )
