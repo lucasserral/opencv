9060ca55cca837f275ba27b41c32822d46372635
Lucas Serral <lucasser21@gmail.com>
2022-12-24 22:06:40 -0300

    [ADD] sections

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,40 @@ import Viewer from "./components/Viewer";
 
 function App() {
   const [values, setValues] = React.useState({});
+  const [sections, setSections] = React.useState([
+    {
+      title: "Experience",
+      sectionItems: [
+        {
+          title: "job 1",
+          description: "Lorem ipsum sit amet.",
+        },
+        {
+          title: "job 2",
+          description: "Lorem ipsum sit amet.",
+        },
+      ],
+    },
+    {
+      title: "Education",
+      sectionItems: [
+        {
+          title: "Institute 1",
+          description: "Lorem ipsum sit amet.",
+        },
+      ],
+    },
+  ]);
 
   return (
     <div className="App">
-      <SideBar values={values} setValues={setValues} />
-      <Viewer values={values} />
+      <SideBar
+        sections={sections}
+        setSections={setSections}
+        values={values}
+        setValues={setValues}
+      />
+      <Viewer values={values} sections={sections} />
     </div>
   );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import OpenCvInput from "./molecules/OpenCvInput";
 
+type sections = Array<{
+  title: string;
+  sectionItems: Array<{ title: string; description: string }>;
+}>;
+
 type props = {
   setValues: Function;
   values: {
     fullname?: string;
+    position?: string;
   };
+  sections: sections;
+  setSections: Function;
 };
 
-export default function SideBar({ values, setValues }: props) {
+export default function SideBar({
+  values,
+  setValues,
+  sections,
+  setSections,
+}: props) {
   return (
     <div className="SideBar">
       <h1>Open CV</h1>
@@ -22,12 +35,89 @@ export default function SideBar({ values, setValues }: props) {
           required={true}
         />
         <OpenCvInput
-          type="paragraph"
-          keyValue={"extra"}
-          placeholder={"extra"}
+          type="shortText"
+          keyValue={"position"}
+          placeholder={"position"}
           setValue={setValues}
           value={values}
+          required={true}
         />
+        <section>
+          <h2>Contact information</h2>
+          <OpenCvInput
+            type="shortText"
+            keyValue="phone"
+            placeholder="phone"
+            setValue={setValues}
+            value={values}
+            required={true}
+          />
+          <OpenCvInput
+            type="shortText"
+            keyValue="email"
+            placeholder="email"
+            setValue={setValues}
+            value={values}
+            required={true}
+          />
+          {/* <OpenCvInput
+            type="shortText"
+            keyValue="email"
+            placeholder="email"
+            setValue={setValues}
+            value={values}
+            required={true}
+          /> */}
+        </section>
+        <section>
+          {sections.map((section) => (
+            <div>
+              <h2>{section.title}</h2>
+              <div>
+                {section.sectionItems.map((sectionItem, index) => (
+                  <div>
+                    <input
+                      placeholder={sectionItem.title || "title"}
+                      value={sectionItem.title}
+                      onChange={(evt) => {
+                        const value = evt.target.value;
+                        setSections((prev: sections) => {
+                          const previowsSections = [...prev];
+                          const prevThisSectItems =
+                            previowsSections[index].sectionItems;
+                          const i = prevThisSectItems.findIndex(
+                            (sect) => sect.title == sectionItem.title
+                          );
+                          // return prev;
+                          prevThisSectItems[i].title = value;
+                          return previowsSections;
+                        });
+                      }}
+                    />
+                    <textarea
+                      placeholder={sectionItem.description || "description"}
+                      value={sectionItem.description}
+                      onChange={(evt) => {
+                        const value = evt.target.value;
+                        setSections((prev: sections) => {
+                          const previowsSections = [...prev];
+                          const prevThisSectItems =
+                            previowsSections[index].sectionItems;
+                          const i = prevThisSectItems.findIndex(
+                            (sect) => sect.title == sectionItem.title
+                          );
+                          // return prev;
+                          prevThisSectItems[i].description = value;
+                          return previowsSections;
+                        });
+                      }}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </section>
       </form>
     </div>
   );
